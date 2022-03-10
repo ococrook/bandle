849348059634563b96300839558dd507b1347c60
Oliver M. Crook <ococrook@gmail.com>
2022-03-10 15:30:51 +0000

    add posteriorEstimates function

--- R/bandle-class.R
+++ R/bandle-class.R
@@ -241,6 +241,14 @@ setMethod("length", "nicheParams",
 setMethod("length", "nicheParams",
           function(x) length(params(x)))
 
+
+##'@param object An instance of appropriate class.
+##'@rdname bandleParams
+posteriorEstimates <- function(object) {
+    stopifnot(inherits(object, "bandleSummary"))
+    object@posteriorEstimates
+}
+
 ##' @rdname bandleParams
 setMethod("posteriorEstimates", "bandleSummary",
           function(x) x@posteriorEstimates)
